58bc270e7384bf0aafb2f95e2e2fd3608e6bfddf
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-03 07:29:41 +0300

    Lab№5.3

---
+++ Lab№5.3/Lab№5.3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+double s(const double t);
+
+int main()
+{
+    double tp, tk;
+    int n;
+    cout << "tp = "; cin >> tp;
+    cout << "tk = "; cin >> tk;
+    cout << "n = "; cin >> n;
+    double dt = (tk - tp) / n;
+    double t = tp;
+
+    cout << "--------------------------" << endl;
+    cout << "|    t    |     s(t)     |" << endl;
+    cout << "--------------------------" << endl;
+
+    while (t <= tk)
+    {
+        double result = s(2 * t + 1) + 2 * s(t * t) + sqrt(s(t));
+        cout << "| " << setw(8) << fixed << setprecision(2) << t
+            << " | " << setw(11) << setprecision(4) << result << " |" << endl;
+        t += dt;
+    }
+    cout << "--------------------------" << endl;
+
+    return 0;
+}
+
+double s(const double x)
+{
+    if (abs(x) >= 1 || x == 0)
+        return (pow(cos(x), 2) + 1) / exp(x);
+    else
+    {
+        double S = 1;
+        double a = 1;
+        for (int k = 0; k <= 4; k++)
+        {
+            a *= pow(2 * x, 2 * k + 1) / (2 * k + 1);
+            S += a / sin(2 * x);
+        }
+        return S;
+    }
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,60 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+double s(const double x)
+{
+    if (abs(x) >= 1 || x == 0)
+        return (pow(cos(x), 2) + 1) / exp(x);
+    else
+    {
+        double S = 1;
+        double a = 1;
+        for (int k = 0; k <= 4; k++)
+        {
+            a *= pow(2 * x, 2 * k + 1) / (2 * k + 1);
+            S += a / sin(2 * x);
+        }
+        return S;
+    }
+}
+
+namespace UnitTest1
+{
+    TEST_CLASS(FunctionSTest)
+    {
+    public:
+
+        TEST_METHOD(TestSForXGreaterThanOrEqualToOne)
+        {
+            double x = 1.5;
+            double expected = (pow(cos(x), 2) + 1) / exp(x);
+            double actual = s(x);
+            Assert::AreEqual(expected, actual, 0.000001);
+        }
+
+        TEST_METHOD(TestSForXEqualToZero)
+        {
+            double x = 0;
+            double expected = 2.0;
+            double actual = s(x);
+            Assert::AreEqual(expected, actual, 0.000001);
+        }
+
+        TEST_METHOD(TestSForXLessThanOne)
+        {
+            double x = 0.5;
+            double expected = 1.0;
+            double a = 1;
+            for (int k = 0; k <= 4; k++)
+            {
+                a *= pow(2 * x, 2 * k + 1) / (2 * k + 1);
+                expected += a / sin(2 * x);
+            }
+            double actual = s(x);
+            Assert::AreEqual(expected, actual, 0.000001);
+        }
+    };
+}
